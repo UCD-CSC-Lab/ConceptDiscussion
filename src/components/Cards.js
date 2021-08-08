5f9a3ca97b7b9cf0e70bfc1c96ff0166dbaf1124
steven0323 <38046774+steven0323@users.noreply.github.com>
2021-08-09 00:05:41 +0800

    update save

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -36,6 +36,7 @@ class Cards extends Component {
             this.props.SetNewJson(myJson);
             return myJson;
         });
+        console.log("Handle saving is completed ...")
             
     }
     componentDidUpdate(prevProps,prevState) {
